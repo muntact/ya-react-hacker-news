65d6d97a3970082e2ce54d26a58dc22f97136aab
Murray Lucas <murray.lucas@orionhealth.com>
2018-01-06 20:14:14 +1300

    Swap Gatsby for create-react-app. '/src' code is merged not fixed

--- gatsby-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    title: `YA React HN`,
-  },
-  pathPrefix: `/ya-react-hacker-news`,
-  plugins: [`gatsby-plugin-react-helmet`, `gatsby-source-hacker-news`],
-}
